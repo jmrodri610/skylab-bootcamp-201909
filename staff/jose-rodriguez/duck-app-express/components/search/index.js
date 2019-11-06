d3e7fe2bf5f4f5f3dc3b851797d756d40e7d7af1
Jose Rodriguez <jechto610@gmail.com>
2019-11-06 18:04:14 +0100

    init duck-app-express

---
+++ staff/jose-rodriguez/duck-app-express/components/search/index.js
@@ -0,0 +1,12 @@
+module.exports = function() {
+    return  `<section class="view search">
+    <h1 class="search__title">Search</h1>
+    <h2 class="search__user">JOSE</h2><button class="search__logout">Logout</button>
+    <form class="search__form" method="POST" action="/results">
+        <span class="search__icon">🐣</span>
+        <input class="search__criteria" type="text" name="query" placeholder="criteria" defaultValue={query} />
+        <button class="search__submit">🔍</button>
+    </form>
+
+</section>`
+}
