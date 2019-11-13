75c70270bd97a9cda44b1595099f6f680d041749
Jose Rodriguez <jechto610@gmail.com>
2019-11-13 13:09:34 +0100

    init logic tasks project

---
+++ staff/jose-rodriguez/tasks/tasks-api/logic/register-user/index.js
@@ -0,0 +1,29 @@
+const validate = require('../../utils/validate')
+const users = require('../../data/users.json')
+const fs = require('fs')
+const path = require('path')
+const { ContentError} = require('../../utils/errors')
+
+module.exports = function (name, surname, email, username, password) {
+    validate.string(name)
+    validate.string.notVoid('name', name)
+    validate.string(surname)
+    validate.string.notVoid('surname', surname)
+    validate.string(email)
+    validate.string.notVoid('e-mail', email)
+    validate.string(username)
+    validate.string.notVoid('username', username)
+    validate.string(password)
+    validate.string.notVoid('password', password)
+
+    return new Promise((resolve, reject) => {
+
+        let exist 
+        if (users.length > 0) exist = users.findIndex(user => user.username === username)
+        if (exist > 0) return reject (new Error(`user with username "${username}" already exists`))
+        
+        users.push({ name, surname, email, username, password })
+
+        fs.writeFile(path.join(__dirname, '../../data/users.json'), JSON.stringify(users), error => error ? reject(error) : resolve())
+    })
+}
