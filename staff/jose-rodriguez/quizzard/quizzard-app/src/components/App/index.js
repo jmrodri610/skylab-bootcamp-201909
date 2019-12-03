0e4f5f814f32d565eab94e78c3d616ecde7f6994
Jose Rodriguez <jechto610@gmail.com>
2019-12-03 09:44:09 +0100

    app logic register&authenticate

--- staff/jose-rodriguez/quizzard/quizzard-app/src/components/App/index.js
+++ staff/jose-rodriguez/quizzard/quizzard-app/src/components/App/index.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './index.sass'
+import Landing from '../Landing'
+import Home from '../Home'
+import Register from '../Register'
+import Login from '../Login'
+import Profile from '../Profile'
+import { Route, withRouter } from 'react-router-dom'
+import { authenticateUser, registerUser } from '../../logic'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default withRouter(function ({ history }) {
+  
+  function handleGoToLanding() { history.push('/') }
 
-export default App;
+  function handleGoToHome() { history.push('/home') }
+
+  function handleGoToRegister() { history.push('/register') }
+
+  function handleGoToLogin() { history.push('/login') }
+
+  function handleGoBack() { history.push('/home') }
+
+  async function handleRegister(name, surname, email, username, password) {
+      try {
+          await registerUser(name, surname, email, username, password)
+
+          history.push('/login')
+      } catch (error) {
+          console.error(error)
+      }
+  }
+
+  async function handleLogin(username, password) {
+      try {
+          const token = await authenticateUser(username, password)
+
+          sessionStorage.token = token
+
+          history.push('/profile')
+      } catch (error) {
+          console.error(error)
+      }
+  }
+  
+
+  return <>
+      <Route exact path="/" render={() => <Landing onHome={handleGoToHome}  />} />
+      <Route path="/home" render={() =>   <Home  onLanding={handleGoToLanding} onRegister={handleGoToRegister} onLogin={handleGoToLogin} />} />
+      <Route path="/register" render={() =>   <Register  onRegister={handleRegister} onBack={handleGoBack} onLogin={handleGoToLogin} />} />
+      <Route path="/login" render={() =>  <Login  onLogin={handleLogin} onBack={handleGoBack} onRegister={handleGoToRegister} />} />
+      <Route path="/profile" render={()=> <Profile />} />
+
+
+  </>
+})
---
+++ staff/jose-rodriguez/quizzard/quizzard-app/src/components/Profile/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './index.sass'
+
+export default function ({ onLogin }) {
+    return <section className="profile">
+        <h2 className="profile__title">Profile</h2>
+        
+    </section>
+}
